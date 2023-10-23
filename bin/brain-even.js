9650f2715edf8d47407af1d92d968b741b6894c5
Ilya Roschikhmarov <ilrosch@mail.ru>
2023-10-23 16:01:31 +0300

    fix code gameParity

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
+// Функия, котрая приветствует пользователя и запоминает его имя
 const getGreeting = () => {
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name?: ');
@@ -8,20 +9,27 @@ const getGreeting = () => {
     return name;
 };
 
+// Имя игрока (пользователя)
 const userName = getGreeting();
 
+// Функция, которая создает случайный вопрос
+const randomQuestion = () => {
+    const question = Math.floor(Math.random() * 100);
+    const correctAnswer = (question % 2 === 0) ? 'yes' : 'no';
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+
+    return [userAnswer, correctAnswer, correctAnswer === userAnswer];
+};
+
+// Основная функция игры
 const gameParity = () => {
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    const questions = [15, 6, 7];
-    const correctAnswers = ['no', 'yes', 'no'];
-
-    for (let i = 0; i < questions.length; i += 1) {
-        console.log(`Question: ${questions[i]}`);
-        const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-        const correctAnswer = correctAnswers[i];
-
-        if (userAnswer === correctAnswer) {
+    for (let i = 0; i < 3; i += 1) {
+        const [userAnswer, correctAnswer, answer] = randomQuestion();
+        if (answer) {
             console.log('Correct!');
         } else {
             console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \n Let's try again, ${userName}!`);
